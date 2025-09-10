e505161343068a63acbf2673c38fc04db83dd5d2
eddieBean <ed.beaman@outlook.com>
2025-09-10 12:45:50 +1000

    making titles

--- hud.gd
+++ hud.gd
@@ -2,3 +2,29 @@ extends CanvasLayer
 
 func update_score(score):
 	$scoreLabel.text = "Score:" + str(score)
+
+func message(message):
+	$mainTitle.show()
+	$mainTitle.text = message
+	$messageTimer.start()
+	await $messageTimer.timeout
+	$mainTitle.hide()
+
+func startMessage():
+	$mainTitle.show()
+	$mainTitle.text = "3"
+	$messageTimer.start()
+	await $messageTimer.timeout
+	
+	$mainTitle.text = "2"
+	$messageTimer.start()
+	await $messageTimer.timeout
+	
+	$mainTitle.text = "1"
+	$messageTimer.start()
+	await $messageTimer.timeout
+	
+	$mainTitle.text = "GOOO"
+	$messageTimer.start()
+	await $messageTimer.timeout
+	$mainTitle.hide()
--- main.gd
+++ main.gd
@@ -9,15 +9,14 @@ func _ready():
 
 func new_game():
 	score = 0
+	$HUD.startMessage()
 	$Bird.start($startPosition.position)
 	$PipeSpawnTimer.start()
 
 func _on_pipe_spawn_timer_timeout() -> void:
 	var pipe = pipe_scene.instantiate()
-	
 	var pipe_spawn_location = $PipePath/PipeSpawnLocations
 	pipe_spawn_location.progress_ratio = randf()
-	
 	pipe.position = pipe_spawn_location.position
 	var velocity = Vector2(-100.0,0.0)
 	pipe.set_velocity(velocity)
@@ -26,10 +25,13 @@ func _on_pipe_spawn_timer_timeout() -> void:
 	
 func _increase_score():
 	score += 1
-	$HUD.update_score(score)
+	$HUD.update_score(score/24)
 
 
 func _on_bird_hit() -> void:
 	get_tree().call_group("pipes", "queue_free")
+	$PipeSpawnTimer.stop()
+	$HUD.message("U died lmao")
+	
 	print("game over")
 	
